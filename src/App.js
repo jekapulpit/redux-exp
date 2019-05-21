dded6dfc7012c93eb9bd4ed9b99f699f0bb91b4f
zheka <q010@bk.ru>
2019-05-21 14:28:40 +0300

    added simple reducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import reducer from "./reducers";
+import { createStore } from "redux"; // импорт из Redux-библиотеки
+const initialState = {  tech: 'alo' } ;
+const store = createStore(reducer, initialState);  // пока невыполнимое действие
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  hellaao(txt) {
+      console.log(this.state.counter);
+  }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>{store.getState().tech}</p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            </a>
+          </header>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default hot(App);
